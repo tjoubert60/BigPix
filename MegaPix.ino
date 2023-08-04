f01dfe545f3dc35b37504063be38638fe1f7456c
modelvincent <33942751+modelvincent@users.noreply.github.com>
2023-08-04 16:59:38 +0200

    Added fire animation
    Added Fire animation on sequence 8 and with http code 'F'

--- MegaPix.ino
+++ MegaPix.ino
@@ -24,15 +24,15 @@
 /*
    MegaPix ESP32 firmware, listen HTTP request or UDP packet & draw pixels
 
-   2023-06-06  v0.1  T. JOUBERT  Donald, 16 color palette
-   2023-06-25  v1.0  T. JOUBERT  Donald & friends 225 color palette
-   2023-07-04  v1.1  T. JOUBERT  ESP32 LED_PIN = 16
-   2023-07-04  v1.2  T. JOUBERT  UDP server
-   2023-07-15  v1.3  T. JOUBERT  Animated images
-   2023-07-21  v1.4  T. JOUBERT  Animations tempo
-   2023-07-21  v1.5  T. JOUBERT  UDP animation
-   2023-07-24  v1.6  T. JOUBERT  Overcome UDP MTU
-   2023-08-04  v1.7  T. JOUBERT  Updated images
+   2023-06-06  v0.1  	T. JOUBERT  Donald, 16 color palette
+   2023-06-25  v1.0  	T. JOUBERT  Donald & friends 225 color palette
+   2023-07-04  v1.1  	T. JOUBERT  ESP32 LED_PIN = 16
+   2023-07-04  v1.2  	T. JOUBERT  UDP server
+   2023-07-15  v1.3  	T. JOUBERT  Animated images
+   2023-07-21  v1.4  	T. JOUBERT  Animations tempo
+   2023-07-21  v1.5  	T. JOUBERT  UDP animation
+   2023-07-24  v1.6  	T. JOUBERT  Overcome UDP MTU
+   2023-08-04  v1.6dev  V. DECAUX   Fire animation
    ================================================================
 
     This code follows the general structure of the Arduino code:
@@ -136,7 +136,7 @@
  
 */
 
-#define Version   "MegaPix-v1.7 (c)TJO 2023"
+#define Version   "MegaPix-v1.6dev (c)TJO 2023"
 
 #include <WiFi.h>            // comment for ESP8266
 //#include <ESP8266WiFi.h>   // uncomment for ESP8266
@@ -144,7 +144,11 @@
 #include <AsyncUDP.h>
 #include "motifsMPX.h"
 
-#define LED_PIN       16
+
+
+
+
+#define LED_PIN       16 		//15 for EBMC_board
 #define NUM_LEDS      512
 #define INITSEQUENCE  0
 #define MAX_INTENSITY 3
@@ -159,6 +163,265 @@ IPAddress subnet(255,255,255,0);
 WiFiServer server(80);  // HTTP server
 AsyncUDP udp;           // UDP receiver
 CRGB leds[NUM_LEDS];    // LED matrix
+
+
+
+
+
+
+#define DISPLAY_TEST  /* define to show test patterns at startup */
+
+/* MATRIX CONFIGURATION -- PLEASE SEE THE README (GITHUB LINK ABOVE) */
+
+#define MAT_TYPE NEOPIXEL   /* Matrix LED type; see FastLED docs for others */
+#define MAT_W   32          /* Size (columns) of entire matrix */
+#define MAT_H   16          /* and rows */
+//#if defined(ESP32) || defined(ESP8266)
+//#define MAT_PIN 13           /* Data for matrix on D3 on ESP8266 */
+//#else
+//#define MAT_PIN 6           /* Data for matrix on pin 6 for Arduino/other */
+//#endif
+#undef  MAT_COL_MAJOR       /* define if matrix is column-major (that is pixel 1 is in the same column as pixel 0) */
+#define MAT_TOP             /* define if matrix 0,0 is in top row of display; undef if bottom */
+#define MAT_LEFT            /* define if matrix 0,0 is on left edge of display; undef if right */
+#define MAT_ZIGZAG          /* define if matrix zig-zags ---> <--- ---> <---; undef if scanning ---> ---> ---> */
+
+#define BRIGHT 64           /* brightness; min 0 - 255 max -- high brightness requires a hefty power supply! Start low! */
+#define FPS 15              /* Refresh rate */
+
+/* MULTI-PANEL CONFIGURATION -- Do not change unless you connect multiple panels -- See README.md */
+/* WARNING -- THIS IS CURRENTLY UNTESTED -- DO NOT ENABLE UNLESS YOU FEEL LIKE BEING MY CRASH TEST MANNEQUIN */
+#undef  MULTIPANEL          /* define to enable multi-panel support */
+#define PANELS_W    1       /* Number of panels wide */
+#define PANELS_H    1       /* Number of panels tall */
+#undef  PANEL_TOP           /* define if first panel is upper-left */
+#undef  PANEL_ZIGZAG        /* define if panels zig-zag */
+/* --- DO NOT CHANGE THESE LINES --- */
+#ifndef MULTIPANEL
+#define PANELS_H 1
+#define PANELS_W 1
+#undef PANEL_TOP
+#undef PANEL_ZIGZAG
+#endif
+
+/* DEBUG CONFIGURATION */
+
+#undef  SERIAL              /* Serial slows things down; don't leave it on. */
+
+/* SECONDARY CONFIGURATION */
+
+
+/* Display size; can be smaller than matrix size, and if so, you can move the origin.
+ * This allows you to have a small fire display on a large matrix sharing the display
+ * with other stuff. See README at Github. */
+const uint16_t rows = MAT_H * PANELS_H;
+const uint16_t cols = MAT_W * PANELS_W;
+const uint16_t xorg = 0;
+const uint16_t yorg = 0;
+
+/* Flare constants */
+const uint8_t flarerows = 2;    /* number of rows (from bottom) allowed to flare */
+const uint8_t maxflare = 8;     /* max number of simultaneous flares */
+const uint8_t flarechance = 50; /* chance (%) of a new flare (if there's room) */
+const uint8_t flaredecay = 14;  /* decay rate of flare radiation; 14 is good */
+
+/* This is the map of colors from coolest (black) to hottest. Want blue flames? Go for it! */
+const uint32_t colors[] = {
+  0x000000,
+  0x100000,
+  0x300000,
+  0x600000,
+  0x800000,
+  0xA00000,
+  0xC02000,
+  0xC04000,
+  0xC06000,
+  0xC08000,
+  0x807080
+};
+const uint8_t NCOLORS = (sizeof(colors)/sizeof(colors[0]));
+
+uint8_t pix[rows][cols];
+CRGB matrix[MAT_H * PANELS_H * MAT_W * PANELS_W];
+uint8_t nflare = 0;
+uint32_t flare[maxflare];
+
+/** pos - convert col/row to pixel position index. This takes into account
+ *  the serpentine display, and mirroring the display so that 0,0 is the
+ *  bottom left corner and (MAT_W-1,MAT_H-1) is upper right. You may need
+ *  to jockey this around if your display is different.
+ */
+#ifndef MAT_LEFT
+#define __MAT_RIGHT
+#endif
+#ifndef MAT_TOP
+#define __MAT_BOTTOM
+#endif
+#if defined(MAT_COL_MAJOR)
+const uint8_t phy_h = MAT_W;
+const uint8_t phy_w = MAT_H;
+#else
+const uint8_t phy_h = MAT_H;
+const uint8_t phy_w = MAT_W;
+#endif
+#if defined(MULTIPANEL)
+uint16_t _pos( uint16_t col, uint16_t row ) {
+#else
+uint16_t pos( uint16_t col, uint16_t row ) {
+#endif
+#if defined(MAT_COL_MAJOR)
+    uint16_t phy_x = xorg + (uint16_t) row;
+    uint16_t phy_y = yorg + (uint16_t) col;
+#else
+    uint16_t phy_x = xorg + (uint16_t) col;
+    uint16_t phy_y = yorg + (uint16_t) row;
+#endif
+#if defined(MAT_LEFT) && defined(MAT_ZIGZAG)
+  if ( ( phy_y & 1 ) == 1 ) {
+    phy_x = phy_w - phy_x - 1;
+  }
+#elif defined(__MAT_RIGHT) && defined(MAT_ZIGZAG)
+  if ( ( phy_y & 1 ) == 0 ) {
+    phy_x = phy_w - phy_x - 1;
+  }
+#elif defined(__MAT_RIGHT)
+  phy_x = phy_w - phy_x - 1;
+#endif
+#if defined(MAT_TOP) and defined(MAT_COL_MAJOR)
+  phy_x = phy_w - phy_x - 1;
+#elif defined(MAT_TOP)
+  phy_y = phy_h - phy_y - 1;
+#endif
+  return phy_x + phy_y * phy_w;
+}
+
+#if defined(MULTIPANEL)
+uint16_t pos(uint16_t col, uint16_t row) {
+#if defined(PANEL_TOP)
+    uint16_t panel_y = PANELS_H - ( row / MAT_H ) - 1;
+#else
+    uint16_t panel_y = row / MAT_H;
+#endif
+    uint16_t panel_x = col / MAT_W;
+#if defined(PANEL_ZIGZAG)
+    if ( ( panel_y & 1 ) == 1 ) {
+        panel_x = PANELS_W - panel_x - 1;
+    }
+#endif
+    uint16_t pindex = panel_x + panel_y * PANELS_W;
+    return MAT_W * MAT_H * pindex + _pos(col % MAT_W, row % MAT_H);
+}
+#endif
+
+uint32_t isqrt(uint32_t n) {
+  if ( n < 2 ) return n;
+  uint32_t smallCandidate = isqrt(n >> 2) << 1;
+  uint32_t largeCandidate = smallCandidate + 1;
+  return (largeCandidate*largeCandidate > n) ? smallCandidate : largeCandidate;
+}
+
+// Set pixels to intensity around flare
+void glow( int x, int y, int z ) {
+  int b = z * 10 / flaredecay + 1;
+  for ( int i=(y-b); i<(y+b); ++i ) {
+    for ( int j=(x-b); j<(x+b); ++j ) {
+      if ( i >=0 && j >= 0 && i < rows && j < cols ) {
+        int d = ( flaredecay * isqrt((x-j)*(x-j) + (y-i)*(y-i)) + 5 ) / 10;
+        uint8_t n = 0;
+        if ( z > d ) n = z - d;
+        if ( n > pix[i][j] ) { // can only get brighter
+          pix[i][j] = n;
+        }
+      }
+    }
+  }
+}
+
+void newflare() {
+  if ( nflare < maxflare && random(1,101) <= flarechance ) {
+    int x = random(0, cols);
+    int y = random(0, flarerows);
+    int z = NCOLORS - 1;
+    flare[nflare++] = (z<<16) | (y<<8) | (x&0xff);
+    glow( x, y, z );
+  }
+}
+
+/** make_fire() animates the fire display. It should be called from the
+ *  loop periodically (at least as often as is required to maintain the
+ *  configured refresh rate). Better to call it too often than not enough.
+ *  It will not refresh faster than the configured rate. But if you don't
+ *  call it frequently enough, the refresh rate may be lower than
+ *  configured.
+ */
+unsigned long t = 0; /* keep time */
+void make_fire() {
+  uint16_t i, j;
+  if ( t > millis() ) return;
+  t = millis() + (1000 / FPS);
+
+  // First, move all existing heat points up the display and fade
+  for ( i=rows-1; i>0; --i ) {
+    for ( j=0; j<cols; ++j ) {
+      uint8_t n = 0;
+      if ( pix[i-1][j] > 0 )
+        n = pix[i-1][j] - 1;
+      pix[i][j] = n;
+    }
+  }
+
+  // Heat the bottom row
+  for ( j=0; j<cols; ++j ) {
+    i = pix[0][j];
+    if ( i > 0 ) {
+      pix[0][j] = random(NCOLORS-6, NCOLORS-2);
+    }
+  }
+
+  // flare
+  for ( i=0; i<nflare; ++i ) {
+    int x = flare[i] & 0xff;
+    int y = (flare[i] >> 8) & 0xff;
+    int z = (flare[i] >> 16) & 0xff;
+    glow( x, y, z );
+    if ( z > 1 ) {
+      flare[i] = (flare[i] & 0xffff) | ((z-1)<<16);
+    } else {
+      // This flare is out
+      for ( int j=i+1; j<nflare; ++j ) {
+        flare[j-1] = flare[j];
+      }
+      --nflare;
+    }
+  }
+  newflare();
+
+  // Set and draw
+  for ( i=0; i<rows; ++i ) {
+    for ( j=0; j<cols; ++j ) {
+      leds[pos(j,i)] = CRGB((colors[pix[i][j]] >> 16)&0x0000FF, (colors[pix[i][j]] >> 8)&0x0000FF, colors[pix[i][j]]&0x0000FF);
+      //Serial.printf("0x%x: 0x%x, 0x%x, 0x%x\n", colors[pix[i][j]], (colors[pix[i][j]] >> 16)&0x0000FF, (colors[pix[i][j]] >> 8)&0x0000FF, colors[pix[i][j]]&0x0000FF);
+      //leds[idpix] = CRGB ( red/2, green/2,  blue/2);
+    }
+  }
+  FastLED.show();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //
 // MPX Display structures
@@ -181,9 +444,24 @@ int UDPfirstSz = 0;     // first UDP packet size
 //
 //  initialize LED, HTTP and UDP
 //
+
+void dumpMem(char* buffer, int nbytes);
+void DrawPalette(char*  motif);
+void AnimateMPX(char* motif);
+void DisplayMPX(char* motif);
+void DrawMPX(char*  motif, int animidx);
+void RandomColor();
+void AnimateLine(int line, int dt);
+void ClearPixel(int li, int co);
+void DoPixel(int li, int co, char red, char green, char blue, char intensite);
+
+
+
 void setup()
 {
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS); // init LED object
+  //FastLED.addLeds<MAT_TYPE, LED_PIN>(matrix, (MAT_H * PANELS_H * MAT_W * PANELS_W));
+  
   Serial.begin(115200);
   Serial.println(Version);
   Serial.println();
@@ -356,6 +634,11 @@ void loop()
             requestDone = 1;
             stepMotif = 0;
           }
+          else if (currentLine.endsWith("GET /F "))  // Fire
+          { sequence = 8;
+            requestDone = 1;
+            stepMotif = 0;
+          }
            
           if (requestDone == 1)     // prepare display
           { imgdone = 0;
@@ -398,11 +681,14 @@ void loop()
     break;
 
   case 6:                           // Perle
-    AnimateMPX(perle);
+    DisplayMPX(perle);
     break;
 
   case 7:                           // UDP guest
     AnimateMPX(udpmotif);
+    break;
+  case 8:                           // fire
+    make_fire();
     break;
   }
 }
@@ -655,6 +941,7 @@ void AnimateMPX(char* motif)
 //
 // Draw first 16 palette enties of MPX image
 //
+
 void DrawPalette(char*  motif)
 {
 int colpix = 0;
@@ -690,3 +977,4 @@ void dumpMem(char* buffer, int nbytes)
   }
   Serial.println();
 }
+
